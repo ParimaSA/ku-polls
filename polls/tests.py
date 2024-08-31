1c77d466df2cf6c720a3865c59eef2ea176be692
Parima SANGSIRAKOUP <parima.sa@ku.th>
2024-08-31 16:09:03 +0700

    add unit tests for is_published and can_vote methods

--- polls/tests.py
+++ polls/tests.py
@@ -1,6 +1,8 @@
 import datetime
 from django.test import TestCase
 from django.utils import timezone
+from fontTools.misc.plistlib import end_date
+
 from .models import Question
 from django.urls import reverse
 
@@ -43,6 +45,63 @@ class QuestionModelTests(TestCase):
         in_one_day = timezone.now() - datetime.timedelta(hours=23, minutes=59, seconds=59)
         recent_question = Question(pub_date=in_one_day)
         self.assertTrue(recent_question.was_published_recently())
+
+    def test_is_published_with_future_question(self):
+        """
+        is_published method returns False
+        for questions whose pub_date is in the future.
+        """
+        future = timezone.now() + datetime.timedelta(minutes=3)
+        future_question = Question(pub_date=future)
+        self.assertFalse(future_question.is_published())
+
+
+    def test_is_published_with_recent_question(self):
+        """
+        is_published method returns True
+        for questions whose pub_date is current date-time
+        """
+        recent = timezone.now()
+        recent_question = Question(pub_date=recent)
+        self.assertTrue(recent_question.is_published())
+
+    def test_is_published_with_old_question(self):
+        """
+        is_published method returns True
+        for questions whose pub_date is older than current date-time
+        """
+        past = timezone.now() - datetime.timedelta(days=2)
+        old_question = Question(pub_date=past)
+        self.assertTrue(old_question.is_published())
+
+    def test_can_vote_with_unpublished_question(self):
+        """
+        can_vote method returns False
+        for questions whose pub_date is in the future
+        """
+        future = timezone.now() + datetime.timedelta(days=1)
+        unpublished_question = Question(pub_date=future)
+        self.assertFalse(unpublished_question.can_vote())
+
+    def test_can_vote_with_closed_question(self):
+        """
+        can_vote method returns False
+        for questions whose end_date is in the past
+        """
+        past = timezone.now() - datetime.timedelta(seconds=1)
+        closed_question = Question(end_date=past)
+        self.assertFalse(closed_question.can_vote())
+
+    def test_can_vote_with_opened_question(self):
+        """
+        can_vote method returns True
+        for questions whose pub_date is in the past and end_date is in the future
+        """
+        future = timezone.now() + datetime.timedelta(days=1)
+        opened_question = Question(end_date=future)
+        self.assertTrue(opened_question.can_vote())
+        opened_question_with_null_end_date = Question()
+        self.assertTrue(opened_question_with_null_end_date.can_vote())
 
 
 class QuestionIndexViewTests(TestCase):
