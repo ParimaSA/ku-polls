de1309d8384049b8c41298492c1414e17d115126
Parima SANGSIRAKOUP <parima.sa@ku.th>
2024-09-08 18:22:45 +0700

    Edit test auth with sample auth tests

--- polls/tests/test_auth.py
+++ polls/tests/test_auth.py
@@ -1,46 +1,95 @@
-from django.test import TestCase
+import django.test
 from django.urls import reverse
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate  # to "login" a user using code
+from polls.models import Question, Choice
+from mysite import settings
 
-class AuthTest(TestCase):
-    """Test for checking the authentication"""
+
+def create_question(question_text):
+    """
+    Create a question with the given 'question_text' and published
+    the given number of 'days' offset to now
+    (negative for questions published in the past,
+     positive for questions that have not published)
+    """
+    return Question.objects.create(question_text=question_text)
+
+
+class UserAuthTest(django.test.TestCase):
 
     def setUp(self):
-        """Create a user for testing purposes."""
-        self.user = User.objects.create_user(username='test', password='1234')
+        # superclass setUp creates a Client object and initializes test database
+        super().setUp()
+        self.username = "test"
+        self.password = "1234"
+        self.user1 = User.objects.create_user(
+            username=self.username,
+            password=self.password,
+        )
+        self.user1.save()
+        # create a poll question to test voting
+        q = create_question(question_text="First Poll Question")
+        q.save()
+        # a few choices
+        for n in range(1, 4):
+            choice = Choice(choice_text=f"Choice {n}", question=q)
+            choice.save()
+        self.question = q
 
-    def test_success_login(self):
+    def test_logout(self):
+        """A user can log out using the logout url.
+        As an authenticated user,
+        when I visit /accounts/logout/
+        then I am logged out
+        and then redirected to the login page.
         """
-        If the user enters the correct username and password,
-        they should be logged in and redirected to the Index page with a greeting context.
-        """
-        login_successful = self.client.login(username='test', password='1234')
-        self.assertTrue(login_successful)
+        logout_url = reverse("logout")
+        # Client.login returns true on success
+        self.assertTrue(
+            self.client.login(username=self.username, password=self.password)
+        )
+        # visit the logout page
+        form_data = {}
+        response = self.client.post(logout_url, form_data)
+        self.assertEqual(302, response.status_code)
 
-        response = self.client.get(reverse('polls:index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertContains(response, 'Welcome back, test')
+        # should redirect us to where? Polls index? Login?
+        self.assertRedirects(response, reverse("polls:index"))
 
-    def test_failed_login(self):
-        """
-        If the user enters incorrect username or password,
-        they should not be able to log in.
-        """
-        # Attempt login with incorrect username
-        login_failed = self.client.login(username='wrong_username', password='1234')
-        self.assertFalse(login_failed)
+    def test_login_view(self):
+        """A user can log in using the login view."""
+        login_url = reverse("login")
+        # Can get the login page
+        response = self.client.get(login_url)
+        self.assertEqual(200, response.status_code)
+        # Can login using a POST request
+        # usage: client.post(url, {'key1":"value", "key2":"value"})
+        form_data = {"username": "test",
+                     "password": "1234"
+                     }
+        response = self.client.post(login_url, form_data)
+        # after successful login, should redirect browser somewhere
+        self.assertEqual(302, response.status_code)
+        # should redirect us to the polls index page ("polls:index")
+        self.assertRedirects(response, reverse(settings.LOGIN_REDIRECT_URL))
 
-        # Attempt login with incorrect password
-        login_failed = self.client.login(username='test', password='wrong_password')
-        self.assertFalse(login_failed)
+    def test_auth_required_to_vote(self):
+        """Authentication is required to submit a vote.
 
-    def test_success_logout(self):
+        As an unauthenticated user,
+        when I submit a vote for a question,
+        then I am redirected to the login page
+        or I receive a 403 response (FORBIDDEN)
         """
-        If the user is logged in and then logs out,
-        they should be redirected to the login page, and login status should be False.
-        """
-        self.client.login(username='test', password='1234')
-        response = self.client.get(reverse('logout'))
-        # Ensure the user is logged out
-        response = self.client.get(reverse('polls:index'))
-        self.assertNotContains(response, 'Welcome back, test')
+        vote_url = reverse('polls:vote', args=[self.question.id])
+
+        # what choice to vote for?
+        choice = self.question.choice_set.first()
+        # the polls detail page has a form, each choice is identified by its id
+        form_data = {"choice": f"{choice.id}"}
+        response = self.client.post(vote_url, form_data)
+        # should be redirected to the login page
+        self.assertEqual(response.status_code, 302)  # could be 303
+        login_with_next = f"{reverse('login')}?next={vote_url}"
+        self.assertRedirects(response, login_with_next)
